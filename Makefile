bd13490872c543e14be755477ad02002d81e8df1
rikioy <rikioy@mydev.lan>
2019-09-25 14:26:08 +0800

    commit

--- Makefile
+++ Makefile
@@ -1,21 +1,22 @@
 BUILD_NAME:=file_archive
 BUILD_VERSION:=1.0
-SOURCE:=./src/*.go
+SOURCE:= ./*.go
 
-all: build_linux build_windows
+build: linux
 
-build_linux:
+all: linux windows
+
+linux:
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${BUILD_NAME} ${SOURCE}
-	cp src/config.ini.example config.ini
+	cp config.ini.example config.ini
 
-build_windows:
+windows:
 	CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -o ${BUILD_NAME}.exe ${SOURCE}
-	cp src/config.ini.example config.ini
+	cp config.ini.example config.ini
 
 clean:
 	rm ${BUILD_NAME}
 	rm ${BUILD_NAME}.exe
 	rm config.ini
 
-
-.PHONY: all build build_linux build_win clean
+.PHONY: all build linux windows clean
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"bufio"
 
 	"github.com/urfave/cli"
 	"gopkg.in/ini.v1"
@@ -17,6 +18,8 @@ var (
 	FfprobeExe string
 	//Cfg 配置文件
 	Cfg         *ini.File
+	//MoveFile 是否移动文件
+	MoveFile bool
 )
 
 func init() {
@@ -32,9 +35,11 @@ func main() {
 	SrcPath = Cfg.Section("").Key("src_path").String()
 	DstPath = Cfg.Section("").Key("dst_path").String()
 	FfprobeExe = Cfg.Section("").Key("ffprobe_exe").String()
+	MoveFile, _= Cfg.Section("").Key("move_file").Bool()
 	log.Printf("src path:%s\n", SrcPath)
 	log.Printf("dst path:%s\n", DstPath)
 	log.Printf("ffprobe_exe:%s\n", FfprobeExe)
+	log.Printf("move_file:%t\n", MoveFile)
 
 	app := cli.NewApp()
 	app.Commands = []cli.Command{
@@ -66,4 +71,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	inputReader := bufio.NewReader(os.Stdin)
+	inputReader.ReadString('\n')
 }
